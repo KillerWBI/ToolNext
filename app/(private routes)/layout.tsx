761b3b3d998af2104aeafb3356f1a13f772da1e9
GetUp <108660418+KillerWBI@users.noreply.github.com>
2025-12-13 12:18:24 +0100

    доп изменение

--- app/(private routes)/layout.tsx
+++ app/(private routes)/layout.tsx
@@ -1,9 +1,9 @@
-import { cookies } from 'next/headers';
 import { redirect } from 'next/navigation';
 import type { ReactNode } from 'react';
+import { verifyAuthServer } from '../../lib/auth';
 
 export default async function PrivateLayout({ children }: { children: ReactNode }) {
-  const token = (await cookies()).get('accessToken')?.value;
+  const token = await verifyAuthServer();
   if (!token) redirect('/auth/login');
   return <>{children}</>;
 }
