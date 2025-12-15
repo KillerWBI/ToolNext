3ce1dceb632992f77feed1dba7f3cf9e02c25024
Marina Prushak <87096097+huruzka@users.noreply.github.com>
2025-12-15 12:56:33 +0200

    benefits block

---
+++ components/home/BenefitsBlock/BenefitsBlock.tsx
@@ -0,0 +1,35 @@
+import css from "./BenefitsBlock.module.css";
+
+export default function BenefitsBlock(){
+    return (
+        <section className={css.benefitsBlock}>
+        <h2 className={css.benefitsTitle}>ToolNext — платформа для швидкої та зручної оренди інструментів</h2>
+        <p className = {css.benefitsText}>ToolNext допомагає знайти потрібний інструмент у декілька кліків.
+            Користувачі можуть легко орендувати обладнання для ремонту чи хобі, а власники — зручно керувати своїми оголошеннями.
+            Ми створили сервіс, щоб зробити процес оренди простим, доступним і вигідним для всіх.</p>
+        <ul className = {css.benefitsLst}>
+                <li className = {css.benefitsListItem}>
+                    <svg className ={css.benefitsSvg} width="48" height = "48">
+                        <use href="../../../public/svg/"/>
+                    </svg>
+                 <h3>Легкий доступ до інструментів</h3>
+                <p>Знаходьте потрібний інструмент у своєму районі без зайвих дзвінків і пошуків. Просто введіть назву — і отримайте варіанти поруч із вами.</p>
+                </li>
+        <li className = {css.benefitsListItem}>
+                    <svg className ={css.benefitsSvg} width="48" height = "48">
+                        <use href="../../../public/svg/"/>
+                    </svg>
+                 <h3>Швидке бронювання</h3>
+                <p>Бронюйте інструменти в кілька кліків. Жодних складних форм чи довгих очікувань — тільки простий та зручний процес.</p>
+                </li >
+                    <li className = {css.benefitsListItem}>
+                    <svg className ={css.benefitsSvg} width="48" height = "48">
+                        <use href="../../../public/svg/"/>
+                    </svg>
+                 <h3>Зручне управління</h3>
+                <p>Додавайте свої інструменти в каталог, редагуйте оголошення та контролюйте оренду. ToolNext допомагає перетворити зайві інструменти на додатковий дохід.</p>
+                </li>
+        </ul >
+        </section>        
+    );
+}
