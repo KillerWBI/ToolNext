52d5fda8e22a2448a9186d4e72bd64f130eab3d2
Valerii Riznyck <valera451reznick@gmail.com>
2025-12-16 14:33:17 +0200

    initial add file

--- app/(public routes)/tools/page.tsx
+++ app/(public routes)/tools/page.tsx
@@ -1,3 +1,15 @@
+import ToolsGrid from "@/components/tools/ToolsGrid/ToolsGrid";
+
+export const metadata = {
+  title: "Усі інструменти | ToolNext",
+  description: "Перегляньте всі доступні інструменти для оренди",
+};
+
 export default function Page() {
-  return null;
+    return (
+        <main>
+            <ToolsGrid />
+        </main>
+    );
 }
+
