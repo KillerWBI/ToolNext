41f387f9be160ae5771e2de3cc4505b4bd74ca98
Lina Khatsko <lina.khatsko@gmail.com>
2025-12-18 17:41:45 +0200

    fix type feedback

--- types/feedback.ts
+++ types/feedback.ts
@@ -1,6 +1,6 @@
-export interface Feedback {
-  id: string;
-  rating: number;
-  text: string;
-  author: string;
-}
+export type Feedback = {
+  _id: { $oid: string };
+  name: string;
+  description: string;
+  rate: number;
+};
