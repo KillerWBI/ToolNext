0f59efd1af4e121c27f13445df14208e492b8a0e
Stanislav Nemchenko <stas.buka@gmail.com>
2025-12-22 00:29:16 +0200

    fix: update package.json and tsconfig.json formatting
    - Added missing newline at the end of package.json
    - Reformatted tsconfig.json for consistent indentation and readability

--- app/(public routes)/tools/[toolId]/page.tsx
+++ app/(public routes)/tools/[toolId]/page.tsx
@@ -3,33 +3,32 @@ import { ToolInfoBlock } from "@/components/tools/ToolInfoBlock/ToolInfoBlock";
 import css from "./page.module.css";
 import { getToolById } from "@/lib/api/tools";
 import { getPublicUserById } from "@/lib/api/users";
-import { useAuthStore } from "@/store/auth.store";
 
 interface DetailsPageProps {
-    params: Promise<{ toolId: string }>;
+  params: Promise<{ toolId: string }>;
 }
 
 export default async function DetailsPage({ params }: DetailsPageProps) {
-    const { toolId } = await params;
+  const { toolId } = await params;
 
-    const tool = await getToolById(toolId);
+  const tool = await getToolById(toolId);
 
-    if (!tool) {
-        return (
-            <div className="container">
-                <p>Інструмент не знайдено.</p>
-            </div>
-        );
-    }
-
-    const owner = await getPublicUserById(tool.owner);
-
+  if (!tool) {
     return (
-        <div className="container">
-            <div className={css.detailsPage}>
-                <ToolGallery tool={tool} />
-                <ToolInfoBlock tool={tool} owner={owner} />
-            </div>
-        </div>
+      <div className="container">
+        <p>Інструмент не знайдено.</p>
+      </div>
     );
+  }
+
+  const owner = await getPublicUserById(tool.owner);
+
+  return (
+    <div className="container">
+      <div className={css.detailsPage}>
+        <ToolGallery tool={tool} />
+        <ToolInfoBlock tool={tool} owner={owner} />
+      </div>
+    </div>
+  );
 }
